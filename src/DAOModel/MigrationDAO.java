975d4680f085cc734707f35d265114954973fbfa
Carlos Salas <devel@reserland.com>
2015-12-05 08:01:17 +0100

    Corrección de error al recuperar directorios

--- src/DAOModel/MigrationDAO.java
+++ src/DAOModel/MigrationDAO.java
@@ -47,38 +47,32 @@ public class MigrationDAO {
         String nameDirectory = null;
         ResultSet rs = null;
         ResultSet rs2 = null;
-        ResultSet rs3 = null;
+
         
         ps1 = con.prepareStatement("SELECT ID_IMAGEN as idimage, NOMBRE as name, EXTENSION as extension, TAMANO as sizes, RUTA.PATH as pat FROM IMAGEN,RUTA WHERE IMAGEN.ID_RUTA=RUTA.ID_RUTA");
         rs = ps1.executeQuery();
         
         while (rs.next()) { // Para cada imagen
             md_list = new ArrayList<>();
-            ps2 = con.prepareStatement("SELECT IMAGEN_ETIQUETA.VALOR as valor, ETIQUETA.NOMBRE AS name, ETIQUETA.ID_ETIQUETA as idEtiqueta FROM IMAGEN_ETIQUETA, ETIQUETA WHERE ID_IMAGEN=? AND ETIQUETA.ID_ETIQUETA=IMAGEN_ETIQUETA.ID_ETIQUETA");
+            ps2 = con.prepareStatement("SELECT DIRECTORIO.NOMBRE AS DIRECTORIO, IMAGEN_ETIQUETA.VALOR as valor, ETIQUETA.NOMBRE AS name, ETIQUETA.ID_ETIQUETA as idEtiqueta FROM IMAGEN_ETIQUETA, ETIQUETA, DIRECTORIO WHERE ID_IMAGEN=? AND ETIQUETA.ID_ETIQUETA=IMAGEN_ETIQUETA.ID_ETIQUETA AND ETIQUETA.ID_DIRECTORIO = DIRECTORIO.ID_DIRECTORIO ORDER BY DIRECTORIO");
             ps2.clearParameters();
             ps2.setInt(1, rs.getInt("idimage"));
             rs2 = ps2.executeQuery();
             vl_list = new ArrayList<>();
             nameDirectory = null;
             while (rs2.next()) { // Para cada etiqueta de la imagen
-                Value_Label valueLabel = new Value_Label(rs2.getString("valor"), rs2.getString("name"));
-                int directoriId = rs2.getInt("idEtiqueta");
-                ps3 = con.prepareStatement("SELECT DIRECTORIO.NOMBRE as name FROM DIRECTORIO, ETIQUETA WHERE DIRECTORIO.ID_DIRECTORIO=ETIQUETA.ID_DIRECTORIO AND ETIQUETA.ID_ETIQUETA=?");
-                ps3.clearParameters();
-                ps3.setInt(1, directoriId);
-                rs3 = ps3.executeQuery();
-                rs3.next();
+                Value_Label valueLabel = new Value_Label(rs2.getString("valor"), rs2.getString("name"));                
                 if (nameDirectory == null) {
-                    nameDirectory = rs3.getString("name");
+                    nameDirectory = rs2.getString("directorio");
                     vl_list.add(valueLabel);
                 } else {
-                    if (nameDirectory.equals(rs3.getString("name")))
+                    if (nameDirectory.equals(rs2.getString("directorio")))
                         vl_list.add(valueLabel);
                     else {
                         MigrationDirectory mdobj = new MigrationDirectory(nameDirectory, vl_list);
                         md_list.add(mdobj);
                         vl_list = new ArrayList<>();
-                        nameDirectory = rs3.getString("name");
+                        nameDirectory = rs2.getString("directorio");
                     }    
                 }
                 
